3f5ec27a4f0c466f885b8c914eb4559ecc5a9db8
ralexrivero <ralexrivero@gmail.com>
2021-09-19 19:57:52 -0400

    add boolean header

--- initilize.c
+++ initilize.c
@@ -1,3 +1,15 @@
 #include "window.h"
 
+int main(void)
+{
+	/* starts up SDL and creates window */
+	bool init(void);
 
+	/* loads media */
+	bool loadMedia(void);
+
+	/* frees media and shuts down SDL */
+	void xclose(void);
+
+	return (0);
+}
--- window.h
+++ window.h
@@ -2,5 +2,12 @@
 #define WINDOW_H
 
 #include <SDL2/SDL.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+/* live cycle in modular */
+bool init(void);
+bool loadMedia(void);
+void xclose(void);
 
 #endif /* WINDOW_H */
